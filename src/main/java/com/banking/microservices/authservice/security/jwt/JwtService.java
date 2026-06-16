d6ab09ddcaed4cbd5758fa766b78b308d537dd24
Balla vijay kumar <ballavijay40@gmail.com>
2026-06-16 22:52:06 +0530

    impleented the jwt token generation

--- src/main/java/com/banking/microservices/authservice/security/jwt/JwtService.java
+++ src/main/java/com/banking/microservices/authservice/security/jwt/JwtService.java
@@ -1,11 +1,15 @@
 package com.banking.microservices.authservice.security.jwt;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
-
+import java.util.Date;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +23,44 @@ public class JwtService {
     }
 
     //generate token
+    public String generateToken(String username,Map<String,Object> claims){
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis()+jwtProperties.getExpiration()))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
 
+
+
+    }
+
+//    //extract username
+//    public String extractUsername(String token) {
+//        return getClaims(token).getSubject();
+//    }
+
+    //validate token
+    public boolean isTokenExpired(String token,String username){
+        String extractedUsername=getClaims(token).getSubject();
+        return extractedUsername.equals(username)
+                && !isTokenExpired(token, username);
+    }
+
+
+
+
+
+
+
+    //extract allclaims
+    public Claims getClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
 
 }
